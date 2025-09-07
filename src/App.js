95457d26ad5e34c57736710a6fc17d9d3065258b
alpha-shaon <alphaultltd@gmail.com>
2025-09-08 01:00:52 +0300

    Render branded dashboard as default

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CEOdashboard from "./modules/ceo-dashboard/Dashboard"; // or your actual entry module
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <CEOdashboard />;
 }
 
 export default App;
